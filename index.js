db80618e0d5fe797db61bed215bb80d99e14894a
qcgm1978 <zhanghl@strongunion.com.cn>
2018-12-23 18:25:08 +0800

    random algorithm

--- index.js
+++ index.js
@@ -6,13 +6,12 @@ $(() => {
             const arr = text.split('')
             const newStr = arr.reduce((accumulator, item) => {
                 if (/\w/.test(item)) {
-                    const color = data[item.toUpperCase()].split(',').reduce((accu, it) => {
-                        const arr = it.split('=')
-                        accu[arr[0].trim()] = arr[1].trim()
-                        return accu
-                    }, {})
-                    const rgb = `${color['red']}%,${color['green']}%,${color['blue']}%`
-                    return accumulator + `<span style='color:rgb(${rgb});'>${item}</span>`
+                    const probability = getProbability()
+                    const colorArr = getColor({ data, item })
+                    const color = colorArr.find((item, index) => {
+                        return probability >= item.val && probability <= colorArr[index + 1].val
+                    }).prop
+                    return accumulator + `<span style='color:${color};'>${item}</span>`
                 } else {
                     return accumulator + item
                 }
@@ -20,6 +19,25 @@ $(() => {
             $('p').html(newStr)
         })
     })
+    const getColor = ({ data, item }) => {
+        return data[item.toUpperCase()].split(',').map(item => ({ prop: item.split('=')[0].trim(), val: +item.split('=')[1].trim() })).sort((a, b) => a.val - b.val)
+    }
+    const getRandomLast = () => +Math.random().toString().slice(-1)
+    const getProbability = () => {
+        let obj = {}, n = 0
+        for (let i = 0; i < 10; i++) {
+            const last = getRandomLast()
+            if (obj[last]) {
+                obj[last]++
+                if (obj[last] > n) {
+                    n = obj[last]
+                }
+            } else {
+                obj[last] = 1
+            }
+        }
+        return 0.1 / n * 100
+    }
 
 
 })
